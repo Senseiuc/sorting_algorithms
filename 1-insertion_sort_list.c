946a586b35074934f6eeedeb340e9508e55854e3
senseiuc <dhinhojboy@gmail.com>
2023-01-20 21:57:11 +0000

    bubble big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - a function that sorts a linked list
+ *@list: the linked list to be sorted
+ *
+ *Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp;
+	
