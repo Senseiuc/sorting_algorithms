66100ecce7a6fe48dcd4efa2e2bf49c1fc4e086b
senseiuc <dhinhojboy@gmail.com>
2023-01-21 08:36:41 +0000

    checks

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,6 +9,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp, *current, *hold;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 	current = *list;
 	while (current)
 	{
